437aabcc6712d839bab2801fd31520a6821cf9c2
duraisamykarthi <44989453+duraisamykarthi@users.noreply.github.com>
2018-12-31 08:45:55 -0800

    Add files via upload

---
+++ Rolling_The_Dice.py
@@ -0,0 +1,18 @@
+import random
+min = 1
+max = 6
+
+roll_again = "yes"
+
+
+def raw_input(param):
+    pass
+
+
+while roll_again == "yes" or roll_again == "y":
+    print("Rolling the dices...")
+    print("The values are....")
+    print(random.randint(min, max))
+    print(random.randint(min, max))
+
+    roll_again = raw_input("Roll the dices again?")
